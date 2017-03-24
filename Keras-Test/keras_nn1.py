3c9fdcfa36669a063b2b0860b6a108ff562f2b10
jiayou <jiayou@github>
2017-03-24 13:45:07 +0800

    train keras nn1 done (MNIST, linux)

--- Keras-Test/keras_nn1.py
+++ Keras-Test/keras_nn1.py
@@ -10,33 +10,50 @@ from keras.layers import Dense, Activation
 from keras.optimizers import SGD
 import numpy as np
 
-import mnist_dataset
+import gzip, struct
+
+def read_data(label_url,image_url):
+    with gzip.open(label_url) as flbl:
+        magic, num = struct.unpack(">II",flbl.read(8))
+        label = np.fromstring(flbl.read(),dtype=np.int8)
+    with gzip.open(image_url,'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII",fimg.read(16))
+        image = np.fromstring(fimg.read(),dtype=np.uint8).reshape(len(label),rows,cols)
+    return (label, image)
+
+
+(train_lbl, train_img) = read_data('datasets/mnist/train-labels-idx1-ubyte.gz','datasets/mnist/train-images-idx3-ubyte.gz')
+(val_lbl, val_img) = read_data('datasets/mnist/t10k-labels-idx1-ubyte.gz','datasets/mnist/t10k-images-idx3-ubyte.gz')
+
 
 model = Sequential()
+model.add(Dense(input_dim=784, units=64))
+model.add(Activation("relu"))
+model.add(Dense(units=10))
+model.add(Activation("softmax"))
 
-#import theano as T
+# model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True),
+              metrics=['accuracy'])
 
-model.add(Dense(output_dim=64, input_dim=784))
-model.add(Activation("sigmoid"))
-model.add(Dense(output_dim=10, input_dim=64))
-model.add(Activation("sigmoid"))
+X_train = train_img.reshape(60000, 784) /255
+X_test = val_img.reshape(10000, 784) /255
 
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-# model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
-
-Y_train, X_train, Y_test, X_test = mnist_dataset.load_data()
-X_train = X_train.reshape(60000, 784)
-X_test = X_test.reshape(10000, 784)
-
-Y_vec = np.zeros([60000, 10]);
+Y_train = np.zeros([60000, 10]);
 for i in range(60000):
-    Y_vec[i,Y_train]=1
+    Y_train[i,train_lbl[i]]=1
+
+Y_test = np.zeros([10000, 10]);
+for i in range(10000):
+    Y_test[i,val_lbl[i]]=1
 
 #
-#model.fit(X_train.T, Y_train.T)
-model.fit(X_train, Y_vec)
+#model.train_on_batch(X_train, Y_train)
+hist = model.fit(X_train, Y_train, shuffle=True, validation_split=0.2, batch_size=50)
 
-#
+res= model.evaluate(X_test, Y_test)
+
 
 #Y_predict = model.predict(X_test)
 #print(Y_predict)
