a8f513a7e535699d99bc9136671853c8bbefd549
jiayou <jiayou@github>
2017-03-25 20:33:43 +0800

    Use expanded MNIST dataset.

---
+++ Keras-Test/expand_dataset.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 20:19:16 2017
+
+TODO:
+3) the filters: how did it work?
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import gzip, struct
+
+def read_data(label_url,image_url):
+    with gzip.open(label_url) as flbl:
+        magic, num = struct.unpack(">II",flbl.read(8))
+        label = np.fromstring(flbl.read(),dtype=np.int8)
+    with gzip.open(image_url,'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII",fimg.read(16))
+        image = np.fromstring(fimg.read(),dtype=np.uint8).reshape(len(label),rows,cols)
+    return (label, image)
+
+
+from PIL import Image
+def image_variants(img2d, R=1):
+
+    im1 = np.zeros(img2d.shape, dtype='uint8')
+    im2 = np.zeros(img2d.shape, dtype='uint8')
+    im3 = np.zeros(img2d.shape, dtype='uint8')
+    im4 = np.zeros(img2d.shape, dtype='uint8')
+    im5 = np.zeros(img2d.shape, dtype='uint8')
+    im6 = np.zeros(img2d.shape, dtype='uint8')
+    im7 = np.zeros(img2d.shape, dtype='uint8')
+    im8 = np.zeros(img2d.shape, dtype='uint8')
+    
+    im0                         =   img2d
+    im1[  0:-R    ,    :    ]   =   img2d[  R:    ,   :   ]
+    im2[  R:      ,    :    ]   =   img2d[  0:-R  ,   :   ]
+    im3[  :       ,   0:-R  ]   =   img2d[  :    ,   R:   ]
+    im4[  :       ,   R:    ]   =   img2d[  :    ,   0:-R ]
+    im5[  0:-R    ,   0:-R  ]   =   img2d[  R:    ,  R:   ]
+    im6[  R:      ,   R:    ]   =   img2d[  0:-R  ,  0:-R ]
+    im7[  R:      ,   0:-R  ]   =   img2d[  0:-R  ,  R:   ]
+    im8[  0:-R    ,   R:    ]   =   img2d[  R:    ,  0:-R ]
+
+    img10 = Image.fromarray(im0)
+    img11 = img10.rotate(10)
+    img12 = img10.rotate(-10)
+    img13 = img10.rotate(20)
+    img14 = img10.rotate(-20)
+    
+    im11 = np.array(img11)
+    im12 = np.array(img12)
+    im13 = np.array(img13)
+    im14 = np.array(img14)
+    
+    return [im0, im1, im2, im3, im4, im5, im6, im7, im8, im11, im12, im13, im14]
+
+
+def expand_dataset(label_set, image_set):
+    NUM_VARIANTS = 13
+    label_size = list(label_set.shape)
+    image_size = list(image_set.shape)
+    label_size[0]*=NUM_VARIANTS
+    image_size[0]*=NUM_VARIANTS          
+    
+    expanded_label = [np.zeros(NUM_VARIANTS) + lbl for lbl in label_set]
+    expanded_image = [image_variants(img2d) for img2d in image_set]
+    
+    new_label_set = np.reshape(expanded_label, label_size)
+    new_image_set = np.reshape(expanded_image, image_size)
+    
+    return new_label_set, new_image_set
+
+def test_image_variants0():
+    img=np.array(list(range(9)), dtype='uint8').reshape((3,3))
+    k = image_variants(img)
+    print(k)
+
+def test_image_variants(img):
+    variant_list = image_variants(img)
+    ax = plt.subplot(111)
+    mm = np.bmat(variant_list)
+    ax.imshow(mm, 'Greys')
+    
+def expanded_mnist_dataset():
+    (train_lbl, train_img) = read_data('datasets/mnist/train-labels-idx1-ubyte.gz','datasets/mnist/train-images-idx3-ubyte.gz')
+    return expand_dataset(train_lbl, train_img)
+    
+if __name__=='__main__':
+
+    # test_image_variants0()
+
+    #% Load Data
+    #(train_lbl, train_img) = read_data('datasets/mnist/train-labels-idx1-ubyte.gz','datasets/mnist/train-images-idx3-ubyte.gz')
+    (val_lbl, val_img) = read_data('datasets/mnist/t10k-labels-idx1-ubyte.gz','datasets/mnist/t10k-images-idx3-ubyte.gz')
+    # test_image_variants(val_img[0])
+
+    new_lbl, new_img = expand_dataset(val_lbl, val_img)
+    for loops in range(10):
+        idx = np.random.randint(0, new_lbl.size, 10)
+        fig = plt.figure()
+        mm = np.hstack(new_img[idx])
+        plt.imshow(mm, 'Greys')
+        plt.xlabel(new_lbl[idx])
+        
--- Keras-Test/keras_nn2.py
+++ Keras-Test/keras_nn2.py
@@ -4,7 +4,11 @@ Created on Thu Mar 23 16:40:30 2017
 
 
 Use CNN model against MNIST dataset.
->99%
+Target:  Correctness > 99%
+
+Advanced topics over CNN
+1) expanded data set (nudge, rotate, ...)
+2) summary of predict result, error plot, etc
 
 """
 
@@ -26,91 +30,120 @@ def read_data(label_url,image_url):
         image = np.fromstring(fimg.read(),dtype=np.uint8).reshape(len(label),rows,cols)
     return (label, image)
 
-def keras_nn2():
-    (train_lbl, train_img) = read_data('datasets/mnist/train-labels-idx1-ubyte.gz','datasets/mnist/train-images-idx3-ubyte.gz')
-    (val_lbl, val_img) = read_data('datasets/mnist/t10k-labels-idx1-ubyte.gz','datasets/mnist/t10k-images-idx3-ubyte.gz')
+#%% Load Data
+(train_lbl, train_img) = read_data('datasets/mnist/train-labels-idx1-ubyte.gz','datasets/mnist/train-images-idx3-ubyte.gz')
+(val_lbl, val_img) = read_data('datasets/mnist/t10k-labels-idx1-ubyte.gz','datasets/mnist/t10k-images-idx3-ubyte.gz')
+
+#%% Expanded Data Set
+from expand_dataset import expanded_mnist_dataset
+(train_lbl, train_img) = expanded_mnist_dataset()
+
+#%% Model
+model = Sequential()
+model.add(Conv2D(16,(3,3),input_shape=(28, 28, 1)))
+model.add(MaxPooling2D())
+model.add(Conv2D(64,(3,3),input_shape=(13, 13, 1)))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Dropout(0.5))
+model.add(Activation("relu"))
+model.add(Dense(units=10))
+model.add(Activation("softmax"))
+
+# model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.summary()
+
+#%% Fit
+
+
+tensor_shape_train = list(train_img.shape)
+tensor_shape_train.append(1)
+tensor_shape_test  = list(val_img.shape)
+tensor_shape_test.append(1)
+
+length_train = tensor_shape_train[0]
+length_test  = tensor_shape_test[0]
+
+X_train = train_img.reshape(tensor_shape_train) /255
+X_test = val_img.reshape(tensor_shape_test) /255
+
+Y_train = np.zeros([length_train, 10], dtype='uint8');
+for i in range(length_train):
+    Y_train[i,train_lbl[i]]=1
+
+Y_test = np.zeros([length_test, 10], dtype='uint8');
+for i in range(length_test):
+    Y_test[i,val_lbl[i]]=1
+
+#
+#model.train_on_batch(X_train, Y_train)
+hist = model.fit(X_train, Y_train, shuffle=True, 
+                 epochs = 30,
+                 validation_split=0.2, batch_size=100)
+
+#%% Test
+Y_predict = model.predict(X_test, batch_size=32, verbose=0)
+predict_lbl = Y_predict.argmax(1)
+errors = np.where(predict_lbl != val_lbl)
+mismatch = errors[0]
+print('\n\n')
+print('Error: ', mismatch.size)
+
+#%% Table View
+
+error_matrix = [[np.sum(np.logical_and(val_lbl==i, predict_lbl==j)) 
+                    for i in range(10)]
+                    for j in range(10)]
+
+plt.figure(1)
+ax = plt.subplot(111)
+ax.xaxis.set_visible(True)
+ax.yaxis.set_visible(True)
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_xlabel('validation')
+ax.set_ylabel('predict')
+ax.set_title(str(mismatch.size))
+ytable = ax.table(cellText = error_matrix,
+          colLabels=list(range(10)),
+          rowLabels=list(range(10)),
+          loc='center',cellLoc='center'
+          )
+
+ytable.scale(0.9,1.6)
+
+#%% Figure View
+
+plt.figure(2)
+for i in range(min(100, mismatch.size)):
+    ax = plt.subplot(10, 10, i+1)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    idx = mismatch[i]
+    img = val_img[idx]
+    ax.imshow(img,'Greys')
+    ax.text(30, 10,str(val_lbl[idx]))
+    ax.text(30, 25,str(predict_lbl[idx]))
+
+#%% execute_and_record():
+time_str = str(datetime.datetime.now())
+
+with open(time_str+'_log.txt', 'w') as file:
+    file.write(time_str+'\n')
+    file.write('Error:'+str(mismatch.size)+'\n\n')
+
+plt.figure(1).set_size_inches(8, 6)
+plt.figure(1).savefig(time_str+'_err.png')
+
+plt.figure(2).set_size_inches(8, 6)
+plt.figure(2).savefig(time_str+'_tbl.png')
+
+for fig in plt.get_fignums():
+    plt.close(fig)
     
-    
-    model = Sequential()
-    model.add(Conv2D(16,(3,3),input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D())
-    model.add(Conv2D(16,(3,3),input_shape=(13, 13, 1)))
-    model.add(MaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Dropout(0.5))
-    model.add(Activation("relu"))
-    model.add(Dense(units=10))
-    model.add(Activation("softmax"))
-    
-    # model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='sgd',
-                  metrics=['accuracy'])
-    
-    print(model.summary())
-    
-    X_train = train_img.reshape(60000,28,28,1) /255
-    X_test = val_img.reshape(10000,28,28,1) /255
-    
-    Y_train = np.zeros([60000, 10]);
-    for i in range(60000):
-        Y_train[i,train_lbl[i]]=1
-    
-    Y_test = np.zeros([10000, 10]);
-    for i in range(10000):
-        Y_test[i,val_lbl[i]]=1
-    
-    #
-    #model.train_on_batch(X_train, Y_train)
-    hist = model.fit(X_train, Y_train, shuffle=True, validation_split=0.2, batch_size=100)
-    
-    res= model.evaluate(X_test, Y_test)
-    
-    Y_predict = model.predict(X_test, batch_size=32, verbose=0)
-    predict_lbl = Y_predict.argmax(1)
-    errors = np.where(predict_lbl != val_lbl)
-    mismatch = errors[0]
-    print('\n\n')
-    print('Error: ', mismatch.size)
-    
-    #%% Table View
-    
-    error_matrix = [[np.sum(np.logical_and(val_lbl==i, predict_lbl==j)) 
-                        for i in range(10)]
-                        for j in range(10)]
-    
-    plt.figure(1)
-    ax = plt.subplot(111)
-    ax.xaxis.set_visible(True)
-    ax.yaxis.set_visible(True)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_xlabel('validation')
-    ax.set_ylabel('predict')
-    ax.set_title(str(mismatch.size))
-    ytable = ax.table(cellText = error_matrix,
-              colLabels=list(range(10)),
-              rowLabels=list(range(10)),
-              loc='center',cellLoc='center'
-              )
-    
-    ytable.scale(0.9,1.6)
-    
-    #%% Figure View
-    
-    plt.figure(2)
-    for i in range(min(100, mismatch.size)):
-        ax = plt.subplot(10, 10, i+1)
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-        idx = mismatch[i]
-        img = val_img[idx]
-        ax.imshow(img,'Greys')
-        ax.text(30, 10,str(val_lbl[idx]))
-        ax.text(30, 25,str(predict_lbl[idx]))
-    
-    
-if __name__ == '__main__':
-    keras_nn2()
     
